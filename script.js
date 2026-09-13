371176dfe5f07d30da1be99b5f1cbe2ace06321f
Regina <muhatiaregina68@gmail.com>
2026-09-13 10:35:30 +0300

    Replace all website imagery with curated Pexels photography

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',
   }
 }));
 
-// Keep all editorial imagery on-brand and prevent any legacy image from appearing.
+// Pexels-only editorial imagery for the entire site.
 const safeImages={
   '.arch-image':'https://images.pexels.com/photos/35644501/pexels-photo-35644501.jpeg?auto=compress&cs=tinysrgb&w=1400',
-  '.image-one':'https://images.pexels.com/photos/22690356/pexels-photo-22690356.jpeg?auto=compress&cs=tinysrgb&w=1200',
-  '.image-two':'https://images.pexels.com/photos/33510852/pexels-photo-33510852.jpeg?auto=compress&cs=tinysrgb&w=900',
-  '.image-three':'https://images.pexels.com/photos/15399447/pexels-photo-15399447.jpeg?auto=compress&cs=tinysrgb&w=900',
-  '.panel-image':'https://images.pexels.com/photos/33136967/pexels-photo-33136967.jpeg?auto=compress&cs=tinysrgb&w=1400',
-  '.story-one':'https://images.pexels.com/photos/6766291/pexels-photo-6766291.jpeg?auto=compress&cs=tinysrgb&w=1000',
-  '.story-two':'https://images.pexels.com/photos/9197358/pexels-photo-9197358.jpeg?auto=compress&cs=tinysrgb&w=1000',
-  '.story-three':'https://images.pexels.com/photos/22690354/pexels-photo-22690354.jpeg?auto=compress&cs=tinysrgb&w=1000'
+  '.image-one':'https://images.pexels.com/photos/36040348/pexels-photo-36040348.jpeg?auto=compress&cs=tinysrgb&w=1200',
+  '.image-two':'https://images.pexels.com/photos/18702683/pexels-photo-18702683.jpeg?auto=compress&cs=tinysrgb&w=1000',
+  '.image-three':'https://images.pexels.com/photos/15399447/pexels-photo-15399447.jpeg?auto=compress&cs=tinysrgb&w=1000',
+  '.panel-image':'https://images.pexels.com/photos/7173014/pexels-photo-7173014.jpeg?auto=compress&cs=tinysrgb&w=1400',
+  '.story-one':'https://images.pexels.com/photos/9197358/pexels-photo-9197358.jpeg?auto=compress&cs=tinysrgb&w=1000',
+  '.story-two':'https://images.pexels.com/photos/5905895/pexels-photo-5905895.jpeg?auto=compress&cs=tinysrgb&w=1000',
+  '.story-three':'https://images.pexels.com/photos/4355400/pexels-photo-4355400.jpeg?auto=compress&cs=tinysrgb&w=1000'
 };
 Object.entries(safeImages).forEach(([selector,url])=>document.querySelectorAll(selector).forEach(el=>{el.style.backgroundImage=`url("${url}")`}));
 
