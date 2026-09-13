afbefb2f3e516efd24598417e73033d80853bc9a
Regina <muhatiaregina68@gmail.com>
2026-09-13 07:26:03 +0300

    Add smooth navigation interactions

---
+++ script.js
@@ -0,0 +1,1 @@
+document.querySelector('.menu-button')?.addEventListener('click',()=>{const nav=document.querySelector('.desktop-nav');nav?.classList.toggle('mobile-open')});document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const el=document.querySelector(a.getAttribute('href'));if(el){e.preventDefault();el.scrollIntoView({behavior:'smooth',block:'start'})}}));
